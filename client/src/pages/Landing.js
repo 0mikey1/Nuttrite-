3c337765d944917496517c2ab7f1289eba8960f7
0mikey1 <mbent015@fiu.edu>
2022-08-26 15:19:57 -0400

    landing page with survey component that creates a userSurverDataObj

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,53 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/landing.css";
+import Features from "../components/features";
+import Survey from "../components/survey";
+import Contact from "../components/contact";
+import { HashLink } from "react-router-hash-link";
 
 function Landing() {
   return (
-    <div className="container-sm d-flex justify-content-center ">
-      <div className="row d-flex align-items-center">
-        <div className="col-7 ">
-          <h1 className="text-white">
-            Where Intelligence <br></br> Meets Nutrition.{" "}
-          </h1>
-          <p className="text-white">
-            Calculate the nutrition your body needs, track your meals,<br></br>{" "}
-            explore healthy recipesmd design your own meal plans.
-          </p>
+    <div>
+      <div className="container-sm d-flex justify-content-center ">
+        <div className="row d-flex align-items-center">
+          <div className="col-7 ">
+            <h1 className="text-white">
+              Where Intelligence <br></br> Meets Nutrition.{" "}
+            </h1>
+            <p className="text-white">
+              Calculate the nutrition your body needs, track your meals,
+              <br></br> explore healthy recipes and design your own meal plans.
+            </p>
 
-          <Link to="/register">
-            <button className=" btn btn-primary" role="button ">
-              Get Started
-            </button>
-          </Link>
-        </div>
+            <HashLink to="/#survey">
+              <button className=" btn btn-primary" role="button ">
+                Get Started
+              </button>
+            </HashLink>
+          </div>
 
-        <div className="col-sm">
-          <img
-            className="img-fluid"
-            src="https://cdn-icons.flaticon.com/png/512/3967/premium/3967391.png?token=exp=1661214635~hmac=bce4955bf4b2a24fe1da168813db7046"
-          />
+          <div className="col-sm">
+            <img
+              className="img-fluid"
+              src="https://cdn-icons-png.flaticon.com/512/3246/3246628.png"
+            />
+          </div>
         </div>
       </div>
-    </div>
 
-    // <div className=" landingPage">
-    //   <div className="headers">
-    //     <h1>
-    //       Where Intelligence <br></br> Meets Nutrition.{" "}
-    //     </h1>
-    //     <p>
-    //       Calculate the nutrition your body needs, track your meals, explore
-    //       healthy recipes, and design your own meal plans.
-    //     </p>
-    //     <div className="btn">
-    //       <Link to="/register">
-    //         <button className="getStartedbtn ">Get Started</button>
-    //       </Link>
-    //     </div>
-    //   </div>
-    //   <img src="https://cdn-icons.flaticon.com/png/512/3967/premium/3967391.png?token=exp=1661214635~hmac=bce4955bf4b2a24fe1da168813db7046" />
-    // </div>
+      <Features />
+      <Survey />
+      <Contact />
+    </div>
   );
 }
 
